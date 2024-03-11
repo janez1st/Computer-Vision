0f8dc9b75d8def7da7ef7a0bc5f3ff97618763a1
Andhikaa <152371382+janez1st@users.noreply.github.com>
2024-03-11 22:02:57 +0700

    OCR

--- OCR.py
+++ OCR.py
@@ -18,63 +18,33 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-# In[5]:
-
-
 IMAGE_PATH = '/content/Liverpool.jpg'
 
 
 # #Print All Text
-
-# In[6]:
-
 
 reader = easyocr.Reader(['en'])
 result = reader.readtext(IMAGE_PATH)
 result
 
 
-# In[8]:
-
-
 result[0]
-
-
-# In[9]:
-
 
 result[0][0]
 
-
-# In[10]:
-
-
 result[0][1]
-
-
-# In[11]:
-
 
 result[0][2]
 
-
-# In[12]:
-
+In[12]:
 
 len(result)
-
-
-# In[13]:
-
 
 for i in range(len(result)-1):
   print(result[i][1])
 
 
 # #Showing Picture
-
-# In[7]:
-
 
 img = cv2.imread(IMAGE_PATH)
 plt.imshow(img)
@@ -83,14 +53,8 @@ plt.show()
 
 # # Text Marking
 
-# In[14]:
-
-
 #Koordinat
 result[0][0]
-
-
-# In[15]:
 
 
 #Pojok kiri atas dan kanan bawah
@@ -99,15 +63,8 @@ bottom_right = tuple(result[0][0][2])
 print(top_left)
 
 
-# In[17]:
-
-
 text = result[0][1]
 text
-
-
-# In[18]:
-
 
 img = cv2.imread(IMAGE_PATH)
 img = cv2.rectangle(img,top_left,bottom_right,(255,0,0),3)
@@ -117,9 +74,6 @@ plt.show()
 
 # # All Text Marking
 
-# In[19]:
-
-
 img = cv2.imread(IMAGE_PATH)
 
 for i in range(len(result)-1):
@@ -127,16 +81,5 @@ for i in range(len(result)-1):
   bottom_right = tuple(result[i][0][2])
   img = cv2.rectangle(img,top_left,bottom_right,(255,0,0),3)
 
-
-# In[20]:
-
-
 plt.imshow(img)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
